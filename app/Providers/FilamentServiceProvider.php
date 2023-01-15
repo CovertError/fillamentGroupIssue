a85b51c3273bb48ba42a5c022d5c296dab578199
coverterror <coverterror@gmail.com>
2023-01-15 20:54:33 +0300

    testing collapsing

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -3,15 +3,24 @@
 namespace App\Providers;
 
 use Filament\Facades\Filament;
+use Filament\Navigation\NavigationGroup;
 use Illuminate\Support\ServiceProvider;
 
 class FilamentServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Filament::registerNavigationGroups([
-            'Shop',
-            'Blog',
-        ]);
+        Filament::serving(function () {
+            Filament::registerNavigationGroups([
+                NavigationGroup::make()
+                    ->label('Blog')
+                    ->icon('heroicon-s-cog')
+                    ->collapsed(),
+                NavigationGroup::make()
+                    ->label('Shop')
+                    ->icon('heroicon-s-cog')
+                    ->collapsed(),
+            ]);
+        });
     }
 }
